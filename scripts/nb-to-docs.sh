b7213982f011a2935d9a3faf468755f1aafb09d6
John <jmad1v07@gmail.com>
2024-02-09 03:32:49 +0000

    more notebooks

--- scripts/nb-to-docs.sh
+++ scripts/nb-to-docs.sh
@@ -10,3 +10,8 @@ jupyter nbconvert --clear-output \
 jupyter nbconvert --clear-output \
   --to notebook --output="/workspaces/advanced-spatial-analysis-3300/docs/notebooks/week-2_1.ipynb" \
   "/workspaces/advanced-spatial-analysis-3300/nb_dev/week-2_1.ipynb"
+
+# Lab 2
+jupyter nbconvert --clear-output \
+  --to notebook --output="/workspaces/advanced-spatial-analysis-3300/docs/notebooks/week-2_2.ipynb" \
+  "/workspaces/advanced-spatial-analysis-3300/nb_dev/week-2_2.ipynb"
