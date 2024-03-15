f7276fb7f9a3b7efa4f42a40bb75b7f8ca996307
John <jmad1v07@gmail.com>
2024-03-15 07:19:43 +0000

    week 4 2

--- scripts/nb-to-docs.sh
+++ scripts/nb-to-docs.sh
@@ -48,6 +48,11 @@ jupyter nbconvert --clear-output \
   --to notebook --output="/workspaces/advanced-spatial-analysis-3300/docs/notebooks/week-4_1.ipynb" \
   "/workspaces/advanced-spatial-analysis-3300/nb_dev/week-4_1.ipynb"
 
+# Lab 2
+jupyter nbconvert --clear-output \
+  --to notebook --output="/workspaces/advanced-spatial-analysis-3300/docs/notebooks/week-4_2.ipynb" \
+  "/workspaces/advanced-spatial-analysis-3300/nb_dev/week-4_2.ipynb"
+
 # Week 5
 # Lab 1
 jupyter nbconvert --clear-output \
